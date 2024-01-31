c97e5c186dbd5738c3c36c8dddf3568c46678cc2
JaY4165 <harsha.gontu@gmail.com>
2024-01-31 16:03:44 +0530

    login form

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import Image from 'next/image';
+import LoginForm from '@/components/LoginForm';
 
 function LogIn() {
-  return <div></div>;
+  return (
+    <div className="grid grid-cols-2 grid-rows-1 w-full pt-5">
+      <div className="w-full px-20">
+        <div className="pt-3 pb-8 text-center">
+          <h1 className="text-3xl">Welcome Back ✨</h1>
+          <p className="">
+            Enter your email and password to access your account
+          </p>
+        </div>
+        <LoginForm />
+      </div>
+      <div className=" overflow-hidden flex items-center justify-center">
+        <Image
+          src="/images/OIG.jpeg"
+          className="object-contain rounded-2xl"
+          width={500}
+          height={300}
+          alt="Login image"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default LogIn;
