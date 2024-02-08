ae69f7242b541250df7d8b456e3be92d23146eaa
JaY4165 <harsha.gontu@gmail.com>
2024-02-08 19:14:41 +0530

    bcrypt and registration

--- src/app/api/register/route.tsx
+++ src/app/api/register/route.tsx
@@ -1,10 +1,50 @@
+import { loginFormSchema } from '@/utils/validations';
 import { type NextRequest } from 'next/server';
+import z from 'zod';
+import { hashPassword } from '@/utils/bcryptHash';
+import { createClient } from '@/utils/supabase/supaBaseServer';
+import { cookies } from 'next/headers';
 
 export async function POST(req: NextRequest) {
-  const { body } = req;
-  // const { username, password } = body;
+  try {
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
+    const body: z.infer<typeof loginFormSchema> = await req.json();
+    const parsedBody = loginFormSchema.safeParse(body);
+    if (!parsedBody.success) {
+      return new Response(JSON.stringify(parsedBody.error), {
+        status: 500,
+      });
+    }
+    const { email, password } = parsedBody.data;
 
-  return Response.json({
-    hello: 'world',
-  });
+    const userAlreadyPresent = await supabase.auth.getUser(email);
+
+    if (userAlreadyPresent) {
+      return new Response(JSON.stringify({ error: 'User already exists' }), {
+        status: 400,
+      });
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password: hashedPassword,
+    });
+
+    if (error) {
+      return new Response(JSON.stringify(error), {
+        status: 500,
+      });
+    }
+
+    return new Response(JSON.stringify(data), {
+      status: 201,
+    });
+  } catch (error: any) {
+    return new Response(JSON.stringify(error), {
+      status: 500,
+    });
+  }
 }
