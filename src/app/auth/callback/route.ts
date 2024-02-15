36d101feda99fbd59a91b00dea1a6ed99834bff3
JaY4165 <harsha.gontu@gmail.com>
2024-02-16 01:22:21 +0530

    code 4

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,28 +1,17 @@
 import { createSupabaseServerClient } from '@/utils/supabase/supaBaseServer';
-import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server'
 
-import type { NextRequest } from 'next/server';
+import type { NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
-  try {
-    const requestUrl = new URL(request.url);
-    const code = requestUrl.searchParams.get('code');
+  const requestUrl = new URL(request.url)
+  const code = requestUrl.searchParams.get('code')
 
-    if (code) {
-      const supabase = createSupabaseServerClient();
-      const { error } = await supabase.auth.exchangeCodeForSession(code);
-
-      if (error) {
-
-        return NextResponse.redirect(`/error?error=${error}$status=500`, 500);
-      }
-      return NextResponse.redirect('/trip-planner');
-    } else {
-
-      return NextResponse.redirect(`/error?error${"Missing code parameter"}$status=400`, 400);
-    }
-  } catch (error) {
-
-    return NextResponse.redirect(`/error?error${"Internal Server Error"}$status=500`, 500);
+  if (code) {
+    const supabase = createSupabaseServerClient()
+    await supabase.auth.exchangeCodeForSession(code)
   }
+
+  // URL to redirect to after sign in process completes
+  return NextResponse.redirect(requestUrl.origin)
 }
