8bdf074cbd5b42b9e87870b2aad2bbd16e8956c5
JaY4165 <harsha.gontu@gmail.com>
2024-02-29 16:02:34 +0530

    working with prisma

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -4,9 +4,29 @@ import AvatarDrop from './AvatarDrop';
 import readUser from '@/utils/supabase/readUser';
 import Link from 'next/link';
 import { Button } from '../ui/button';
+import prisma from '@/lib/db';
 
 async function Navbar() {
   const { user } = await readUser();
+  console.log(user);
+  if (user?.data?.user?.email) {
+    const userExists = await prisma.profiles.findUnique({
+      where: {
+        email: user?.data?.user?.email,
+        user_id: user?.data?.user?.id,
+      },
+    });
+
+    if (!userExists) {
+      await prisma.profiles.create({
+        data: {
+          email: user?.data?.user?.email as string,
+          user_id: user?.data?.user?.id,
+        },
+      });
+    }
+  }
+
   return (
     <header className="container z-50">
       <nav className="flex w-full justify-between items-center pt-4 z-50">
